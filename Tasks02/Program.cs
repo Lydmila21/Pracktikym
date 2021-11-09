b93cc9a9fc7702bc389ff0387111ceddb77d86d0
Lydmila <lydmilka.i@mail.ru>
2021-11-09 16:32:58 +0400

    now zadanie 2

---
+++ Tasks02/Program.cs
@@ -0,0 +1,21 @@
+﻿double a, b;
+string s = Console.ReadLine();
+string y = Console.ReadLine();
+a = Convert.ToInt32(s);
+b = Convert.ToInt32(y); 
+
+if  (a == b*b)
+{ 
+    
+    string result = "a * b  равно b";
+    Console.WriteLine( result );
+
+} 
+
+else 
+{
+    result = ("a * b не равно b");
+    Console.WriteLine(result("a * b не равно b"));
+}
+
+
