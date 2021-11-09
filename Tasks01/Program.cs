53f06762da43a28c60ed036ab98e64f4340dae0a
Lydmila <lydmilka.i@mail.ru>
2021-11-09 16:31:26 +0400

    sdelannoe zadanie 1

---
+++ Tasks01/Program.cs
@@ -0,0 +1,15 @@
+﻿ // Первый метод
+/*
+int a = 9;
+Console.WriteLine(a * a);
+
+ Второй (разбирали на семинаре)*/
+
+double a;
+string s = Console.ReadLine();
+a = Convert.ToInt32(s);
+Console.WriteLine(Math.Pow(a, 2));
+
+
+
+
