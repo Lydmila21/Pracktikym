bc3dc0231479db7f281e8f57e2238c039f824023
Lydmila <lydmilka.i@mail.ru>
2021-11-11 16:20:10 +0400

    zadanie

---
+++ Tasks06/Program.cs
@@ -0,0 +1,8 @@
+﻿// Выяснить, является ли число четным
+double x;
+Console.WriteLine("Введите число x: ");
+x = Convert.ToInt32(Console.ReadLine());
+while(x %2 ==0)
+{
+    if(Console.WriteLine("Число является четным:"));
+}
