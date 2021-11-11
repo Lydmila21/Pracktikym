565c75fb7d39ebb990a2c9f4a7108706696ad0b9
Lydmila <lydmilka.i@mail.ru>
2021-11-11 16:21:44 +0400

    zadanie

---
+++ Tasks14/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти третью цифру числа, или сказать что ее нет.
+int x;
+Console.WriteLine( "Введите число :");
+x = Convert.ToInt32(Console.ReadLine());
+if (x % 10)
+{   
+     (Console.WriteLine("Искомое число"));
+}
+else
+{
+    Console.WriteLine("Число является меньше требуемоего");
+}
