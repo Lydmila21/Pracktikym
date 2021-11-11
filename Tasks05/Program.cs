28bfe75727367b78feb24cd852945b44fa0b1ab8
Lydmila <lydmilka.i@mail.ru>
2021-11-11 16:19:24 +0400

    zadanie

---
+++ Tasks05/Program.cs
@@ -0,0 +1,8 @@
+﻿// Вычисление функции y=f(a)
+double y, f, a;
+Console.WriteLine("Введите значение f");
+f = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение a");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(system.Double(y = f(a)));
+
