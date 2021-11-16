e090bacea7460953917adaa1b04f5276f8a7add0
Lydmila <lydmilka.i@mail.ru>
2021-11-16 16:14:50 +0400

    не получается вывести остаток

---
+++ Tasks13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Выяснить, кратно ли число заданному, если нет, то вывесть остаток
+
+
+Console.WriteLine("Введите число для проверки ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите предпологаемое кратное");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(a / b >= 0);
+if (a / b ==0)
+{
+     
+     Console.WriteLine("Число кратно");
+
+}
+
+if (a / b > 0)
+
+{
+    Console.WriteLine( " a / b =");
+}
