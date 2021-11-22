a11387ddcc9de858b06588182fb1d4da7698b4fe
Lydmila <lydmilka.i@mail.ru>
2021-11-22 13:25:03 +0400

    не получилось..

---
+++ Tasks15/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число. Проверить, кратно ли оно 7 и 23.
+
+Double x;
+x = Convert.ToInt32(Console.ReadLine( "Введите число "));
+
+if (x%7==0 & x/23==0)
+{
+    Console.WriteLine("Значение кратно 7 и 23");
+}
+else 
+{
+    Console.WriteLine(" Значение не кратно");
+}
