5da3a4d3168d108690b0cfc227d0c6b9826800a3
Lydmila <lydmilka.i@mail.ru>
2021-11-24 11:26:17 +0400

    yes!

---
+++ Tasks07/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать числа от  -N до N.
+
+Console.WriteLine(" Введите число N ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Числовой отрезок от -N до N");
+int index = -N;
+while (index < N)
+{
+    Console.WriteLine(index++ );
+    
+}
+
