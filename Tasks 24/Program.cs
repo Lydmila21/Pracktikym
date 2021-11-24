d3ab6488ee6e279481b4ef04e03837f2cfda1636
Lydmila <lydmilka.i@mail.ru>
2021-11-24 16:18:14 +0400

    no

---
+++ Tasks 24/Program.cs
@@ -0,0 +1,27 @@
+﻿// Найти кубы чисел от 1 до N
+/*Console.WriteLine("Введите число ");
+int N=Convert.ToInt32(Console.ReadLine());
+int [] arr ={1, N};
+
+
+for( int i = 0; i<N; i++ )
+{
+    int sum = arr(i)*arr(i)*arr(i);
+    Console.WriteLine(sum);
+}
+Вторая часть кода подсмотренна и немного отредактированна.
+ В оригенале был указанн размер массива, моя попытка
+  откоррктировать не совсем удалась. Не понимаю почему не 
+  выводит весь размер, а только начало и конец...
+*/
+
+Console.WriteLine("Введите желаемую длинну массива от 1 до ");
+int N= Convert.ToInt32(Console.ReadLine());
+int[] arr = {1, N};
+
+for(int i = 0; i < arr.Length;i++)
+{
+    int sum = arr[i] *arr[i] * arr[i];
+
+    Console.WriteLine($"куб числа {arr[i]} = {sum}");
+}
