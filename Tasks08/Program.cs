0100bf75e53eb80137bb3281428753c3e5bb0b90
Lydmila <lydmilka.i@mail.ru>
2021-11-24 12:40:22 +0400

    yes

--- Tasks08/Program.cs
+++ Tasks08/Program.cs
@@ -1,24 +1,14 @@
-/*int[] Array;
-Array = ar(1, N);
-N = Console.ReadLine("Введите окончание отрезка");
-index=0;
-if (index< ar)
-{
-    Console.WriteLine((index%2==0),"Вывести все четные числа");
-    index++;
-}*/
+﻿// Показать четные числа от 1 до N.
 
-int N;
-Console.WriteLine("Введите число N");
-N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
 int index = 1;
 while (index < N)
 {
-    if  (index % 2 == 0)
-    { 
-      
-        Console.WriteLine(index);            
-        index++;
+    if (index %2 ==0)
+ 
+    {
+        Console.WriteLine( index );
     }
-   
+    index++;
 }
