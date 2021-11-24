296b0660eb46ab9833b4bb643a42829663fb5734
Lydmila <lydmilka.i@mail.ru>
2021-11-24 15:31:47 +0400

    yes-no

---
+++ Tasks25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти сумму чисел от 1 до А.
+
+Console.WriteLine( "Введите число   "); 
+int A = Convert.ToInt32(Console.ReadLine());
+ 
+for (int index=0; index < A;++ index)
+{
+    int result = 1;
+    result = A + index;
+    Console.WriteLine(result );
+
+}     
+
+   
+    
+  
+
